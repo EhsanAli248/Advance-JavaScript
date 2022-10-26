d5fbb0b6e9493142fdec9b23662a49fbe856ada7
EhsanAli248 <ehsanalia555@gamil.com>
2022-10-26 06:16:20 -0700

    intial

---
+++ modules/liberary.js
@@ -0,0 +1,10 @@
+export let msg="welcome";
+export function user(){
+    console.log("Hello World  ");
+}
+export class test{
+    constructor(){
+        console.log("test class constructor");
+    }
+
+}
---
+++ modules/main.js
@@ -0,0 +1,9 @@
+import{msg,user,test} from "./liberary.js";
+// import * as simple from "./liberary.js";
+// console.log(simple.msg);
+// simple.user();
+// let a=new simple.test();
+
+console.log(msg);
+user();
+let a=new test();
